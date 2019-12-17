415eaa7fb02b0483738b89c5497b9da4abcb4c42
IKERRIZ <okothfaith94@gmail.com>
2019-12-17 11:09:56 +0300

    Registered the application models

--- parl/admin.py
+++ parl/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Image,Comment,Likes,Profile
+
 
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Likes)
+admin.site.register(Profile)
